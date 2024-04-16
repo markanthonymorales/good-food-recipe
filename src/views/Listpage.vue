10c43e791f9e0699fb2f2eb74af32cb10716d4f7
Mark Anthony Morales <mmorales@sourcewell.global>
2024-04-16 19:49:23 +0800

    [Footer] Adding footer to all page

--- src/views/Listpage.vue
+++ src/views/Listpage.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
     import Navigation from "./components/Navigation.vue";
+    import FooterNav from "./components/FooterNav.vue";
 </script>
 <template>
  <div id="listpage" class="h-full w-full container mx-auto font-inter p-2">
@@ -8,8 +9,7 @@
         <header class="text-[17px] text-black tracking-[-0.41px] leading-[22px] font-sf-pro-display font-medium md:text-[28px] md:font-normal md:leading-[33.41px] md:tracking-[0.36px]">List of All Recipes</header>
         <section class="mx-2">
         </section>
-        <footer class="">
-        </footer>
+        <FooterNav />
     </article>
  </div>
 </template>
---
+++ src/views/components/FooterNav.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  const yearToday: number = new Date().getFullYear();
+</script>
+<template>
+    <div class="w-full bg-white flex items-center justify-center py-3">
+      <router-link to="/terms-condition" class="cursor-pointer mx-2">
+        <span>
+          Terms and Conditions
+        </span>
+      </router-link> | 
+      <router-link to="/privacy-policy" class="cursor-pointer mx-2">
+        <span>
+          Privacy Policy
+        </span>
+      </router-link>
+    </div>
+    <div class="w-full w-full bg-white flex items-center justify-center py-3">
+      <span>
+        &copy; Copyrights <span id="year" class="text-info font-black">{{ yearToday }}</span> by Anon. All rights reserved.
+      </span>
+    </div>
+  </template>
+  
