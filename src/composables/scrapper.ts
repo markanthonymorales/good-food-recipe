c2196b11a05f3593883b3a286cff4e58a7f76f00
Mark Anthony Morales <mmorales@sourcewell.global>
2024-04-18 20:22:16 +0800

    [FEATURE] Initialized add recipe instruction code

--- src/composables/scrapper.ts
+++ src/composables/scrapper.ts
@@ -10,8 +10,19 @@ const useScrapper = async() => {
     lists.value.forEach((list: any) => {
         list.links.forEach(async(link: string) => {
             console.log(link);
-            response.value = await axios.get(link);
+            response.value = await axios.get(link) as HTMLElement;
+            // TODO: Apply scrapper
             console.log(response.value.data);
+            // TODO: Apply scrapper add recipe per loop
+            // Alternative: use bulkAdd from Dexie Component
+            // await db.recipes.add({
+            //     title: '',
+            //     instructions: [],
+            //     ingredients: [],
+            //     nutrients: [],
+            //     notes: [],
+            //     source: link,
+            // });
         });
     });
 }
